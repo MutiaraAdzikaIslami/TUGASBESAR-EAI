3ba5b6b131a0a04ee08f3c603620cd912969bb5f
rayindarari <rayindarari@gmail.com>
2026-06-14 13:04:51 +0700

    Add FineService class with getFineInfo method

---
+++ iae-fine-service/FineService.java
@@ -0,0 +1,11 @@
+package com.example.fineservice;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class FineService {
+
+    public String getFineInfo() {
+        return "Fine Service Running";
+    }
+}
